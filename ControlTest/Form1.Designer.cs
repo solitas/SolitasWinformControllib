6e22e93f560616c6ca9f681e8b90191c99d8bae0
노수장 <solitasroh@gmail.com>
2015-09-24 21:29:28 +0900

    AdvencedLabel 추가

--- ControlTest/Form1.Designer.cs
+++ ControlTest/Form1.Designer.cs
@@ -41,7 +41,7 @@
             this.customTabControl1.Location = new System.Drawing.Point(0, 0);
             this.customTabControl1.Name = "customTabControl1";
             this.customTabControl1.SelectedIndex = 0;
-            this.customTabControl1.Size = new System.Drawing.Size(1019, 424);
+            this.customTabControl1.Size = new System.Drawing.Size(391, 467);
             this.customTabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -50,7 +50,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 26);
             this.tabPage1.Margin = new System.Windows.Forms.Padding(0);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Size = new System.Drawing.Size(1011, 394);
+            this.tabPage1.Size = new System.Drawing.Size(383, 437);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             // 
@@ -60,7 +60,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 26);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1011, 394);
+            this.tabPage2.Size = new System.Drawing.Size(383, 437);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             // 
---
+++ Controllib/AdvencedLabel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Controllib
+{
+    public class AdvencedLabel : Control
+    {
+        private string _text;
+
+        public string Text
+        {
+            set
+            {
+                if (!value.Equals(_text))
+                {
+                    _text = value;
+                }
+                Invalidate();
+            }
+            get
+            {
+                return _text;
+            }
+        }
+
+        public AdvencedLabel() :base()
+        {
+            _text = string.Empty;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            
+        }
+    }
+}
