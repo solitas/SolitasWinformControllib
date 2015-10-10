b4e67bdaad884fbf4674195506cff54920fe74c3
노수장 <solitasroh@gmail.com>
2015-10-10 14:24:04 +0900

    TextRender  수정중

--- ControlTest/Form1.Designer.cs
+++ ControlTest/Form1.Designer.cs
@@ -29,72 +29,33 @@
         private void InitializeComponent()
         {
             this.designedPanel1 = new Controllib.DesignedPanel();
-            this.customTabControl1 = new Controllib.CustomTabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.customTabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // designedPanel1
             // 
+            this.designedPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.designedPanel1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.designedPanel1.Location = new System.Drawing.Point(408, 26);
+            this.designedPanel1.Location = new System.Drawing.Point(0, 0);
             this.designedPanel1.Name = "designedPanel1";
-            this.designedPanel1.Size = new System.Drawing.Size(613, 145);
+            this.designedPanel1.Size = new System.Drawing.Size(447, 120);
             this.designedPanel1.TabIndex = 2;
             this.designedPanel1.Text = "Title";
-            // 
-            // customTabControl1
-            // 
-            this.customTabControl1.Controls.Add(this.tabPage1);
-            this.customTabControl1.Controls.Add(this.tabPage2);
-            this.customTabControl1.Location = new System.Drawing.Point(0, 0);
-            this.customTabControl1.Name = "customTabControl1";
-            this.customTabControl1.SelectedIndex = 0;
-            this.customTabControl1.Size = new System.Drawing.Size(391, 467);
-            this.customTabControl1.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.BackColor = System.Drawing.Color.DarkRed;
-            this.tabPage1.Location = new System.Drawing.Point(4, 26);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(0);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Size = new System.Drawing.Size(383, 437);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.BackColor = System.Drawing.Color.MistyRose;
-            this.tabPage2.Location = new System.Drawing.Point(4, 26);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(383, 437);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1073, 479);
+            this.ClientSize = new System.Drawing.Size(447, 248);
             this.Controls.Add(this.designedPanel1);
-            this.Controls.Add(this.customTabControl1);
             this.Name = "Form1";
             this.Text = "test";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.customTabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Controllib.CustomTabControl customTabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
         private Controllib.DesignedPanel designedPanel1;
     }
 }
--- Controllib/Graphic/TextRender.cs
+++ Controllib/Graphic/TextRender.cs
@@ -14,46 +14,24 @@ namespace Controllib.Graphic
         {
             if (bounds.Width == 0 || bounds.Height == 0)
                 return;
+
             var state = g.Save();
 
             GraphicsPath gPath = new GraphicsPath();
             SizeF stringSize = g.MeasureString(s, font);
             float emSize = font.SizeInPoints * 96.0f / 72.0f;
             gPath.AddString(s, font.FontFamily, (int)font.Style, emSize, bounds, format);
-
-
+            
             RectangleF pathRect = gPath.GetBounds();
 
-            float ratio = Math.Min((bounds.Width / pathRect.Width), (bounds.Height / pathRect.Height));
+            float ratio = Math.Max((pathRect.Width / bounds.Width), (pathRect.Height / bounds.Height));
 
-            //g.TranslateTransform(-bounds.X, -bounds.Y);
-
-            if (ratio > 1)
-            {
-                using (Matrix matrix = new Matrix())
-                {
-                    matrix.Scale(ratio, ratio);
-                    gPath.Transform(matrix);
-                    matrix.Translate(-pathRect.X, -pathRect.Y);
-                    matrix.Reset();
-                }
-            }
             g.DrawRectangle(Pens.Black, bounds);
+            //g.TranslateTransform(-bounds.X, -bounds.Y);
+            g.ScaleTransform(ratio + 1, ratio + 1);
+            
             g.FillPath(brush, gPath);
             g.Restore(state);
-        }
-
-        public static void DrawString()
-        {
-
-        }
-
-        private static void debugGraphics(string text, SizeF size, float emSize, Rectangle rect)
-        {
-            Console.WriteLine(text);
-            Console.WriteLine("String Size : {0}", size);
-            Console.WriteLine("EMSIZE : {0}", emSize);
-            Console.WriteLine("Bounds : {0}", rect);
         }
     }
 }
